e0a9dc5f093e4772a1a2bb377358b827d7acfb9a
Bububuka <savinkova.vika@mail.ru>
2022-05-21 14:25:57 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        int cost = 2888;
+        int costForMile = 20;
+        int bonus = cost / costForMile;
+        System.out.println("Количество миль " + bonus);
+    }
+}
